9fe01ce43f9c9f4b8c95504b825b2e31a6314630
Matthew Negasi <negasim@spu.edu>
2023-12-15 18:29:29 -0800

    Trying something now.

--- gpa/vite.config.ts
+++ gpa/vite.config.ts
@@ -5,14 +5,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "build",
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
       find: "./runtimeConfig",
       replacement: "./runtimeConfig.browser",
     },
+  },
+  build: {
+    outDir: "build",
   },
 });
