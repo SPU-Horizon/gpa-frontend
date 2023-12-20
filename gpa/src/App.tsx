4614aa9b81435bbdfc7daef9002c2e2d2d60b4a4
Matthew Negasi <negasim@spu.edu>
2023-12-19 21:12:48 -0800

    Fixing Build.

--- gpa/src/App.tsx
+++ gpa/src/App.tsx
@@ -1,4 +1,3 @@
-// import { useState } from "react";
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
--- gpa/src/sections/landingpage/VisionSection.tsx
+++ gpa/src/sections/landingpage/VisionSection.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function VisionSection() {
   return (
     <div>
