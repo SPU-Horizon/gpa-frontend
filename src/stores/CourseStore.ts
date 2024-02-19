b14bd3b399348561e24feea6fd9a442aac1daab7
AAA2spu <113478193+AAA2spu@users.noreply.github.com>
2024-02-18 19:15:50 -0800

    Quick fix on incorrect import

--- src/stores/CourseStore.ts
+++ src/stores/CourseStore.ts
@@ -1,6 +1,6 @@
 import { StateCreator, create } from 'zustand';
 import axios from 'axios';
-import { useAuthStore } from './AuthStore';
+import useAuthStore from './AuthStore';
 import { get } from 'http';
 import { persist } from 'zustand/middleware';
 
@@ -10,43 +10,6 @@ type CourseStore = {
     initializeClassList: () => void;
     classList : [];
 };
-
-
-
-
-/*
-export const useCourseStore = create<CourseStore>((set) => ({
-    getEnrollments: async () => {
-
-        const email = useAuthStore.getState().email;
-        console.log(email);
-       
-        const res = await axios.get(`http://localhost:3000/course/getCourses?email=${email}`)
-            .then((response) => {
-                console.log(response.data);
-                return response.data;
-            })
-            .catch((err) => {
-                console.log(err);
-                return [];
-            })
-    
-
-        return res   
-    },
-
-
-  // Initialize classList with an empty array
-  classList: [],
-
-  // Call getEnrollments and set classList
-  initializeClassList: async () => {
-    const enrollments = await useCourseStore.getState().getEnrollments();
-    set({ classList: enrollments });
-  },
-
-}));
-*/
 
 
 const useCourseStoreTemplate: StateCreator<
