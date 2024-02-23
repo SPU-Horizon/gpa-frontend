97be166d81dd0b7a98ed9468aac9330cfbd92d77
Kaddijatou Baldeh <baldehk@spu.edu>
2024-02-23 10:42:17 -0800

    Added UserStore to index.ts

--- src/components/dashboard/home/Header.tsx
+++ src/components/dashboard/home/Header.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { UserStore } from "@/stores"; 
+import { useCourseStore } from "@/stores";
 
-export default function Header({ username = "Kaddijah Baldeh" }) {
+export default function Header() {
+
+  const userStore = UserStore();
+  const username = "userStore.username";
+
   return (
     <header className="bg-white dark:bg-black-base p-[.88rem] flex justify-between items-center">
       <h1 className="text-xl font-semibold">Dashboard</h1>
-      <div className="text-xl">Users Name</div>
+      <div className="text-xl">{username}</div>
     </header>
   );
 }
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,3 +1,5 @@
 export { default as useThemeStore } from "./ThemeStore";
 export { default as useCourseStore } from "./CourseStore";
 export { default as useUserStore } from "./UserStore";
+export { default as UserStore } from "./CourseStore";
+export { default as HomeStore } from "./CourseStore";
