af5ec5b05ee22171a84fe16a1751457b62dd45f6
Matthew Negasi <negasim@spu.edu>
2024-03-06 14:34:44 -0800

    Fixing NavBar issue.

--- src/components/custom/NavBar.tsx
+++ src/components/custom/NavBar.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { LucideIcon } from "lucide-react";
-
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "../ui/button";
 import { Tooltip, TooltipTrigger, TooltipContent } from "../ui/tooltip";
