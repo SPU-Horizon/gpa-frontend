1e13fc738962d3d46622a21011344d91a95aacd0
Kaddijatou Baldeh <baldehk@spu.edu>
2024-05-16 11:06:40 -0700

    adding statement for field seperation

--- src/pages/BannerIntegration.tsx
+++ src/pages/BannerIntegration.tsx
@@ -120,7 +120,7 @@ export default function IntegrationPage() {
           <div className="font-avenir flex flex-col gap-2">
             <div>
               <Text className="font-bold text-base text-center">
-                We've noticed there are some missing fields: {res.missingFields.join(" ")}.
+                We've noticed there are some missing fields: {res.missingFields.join("")}.
                 Would you like to upload additional files to complete these entries?
               </Text>
             </div>
